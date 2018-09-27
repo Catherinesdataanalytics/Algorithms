6a6fa6e9026e59886da44920ee7abacda46e0a68
Catherine Shen <chuqiao.shen@rhsmith.umd.edu>
2018-09-27 10:35:02 -0400

    Create reversePairs.py

---
+++ Lintcode/reversePairs.py
@@ -0,0 +1,42 @@
+class Solution:
+    """
+    @param A: an array
+    @return: total of reverse pairs
+    """
+    def reversePairs(self, A):
+
+        self.tmp = [0]*len(A)
+        return self.mergeSort(A, 0 , len(A) - 1 )
+                
+                
+    def mergeSort(self, A, l, r):
+        if l >= r:
+            return 0 
+            
+        m = (l + r) >>1 
+        ans = self.mergeSort(A,l,m) + self.mergeSort(A, m+1, r)
+        i = l 
+        j = m + 1 
+        k = l 
+        while i <= m and j <= r:
+            if A[i] > A[j]:
+                self.tmp[k] = A[j]
+                j += 1 
+                ans += m - i + 1 
+            else:
+                self.tmp[k] = A[i]
+                i += 1 
+            k += 1 
+            
+        while i <= m:
+            self.tmp[k] = A[i]
+            k += 1 
+            i += 1 
+        while j <= r:
+            self.tmp[k] = A[j]
+            k += 1 
+            j += 1 
+        for i in range(l, r+1):
+            A[i] = self.tmp[i]
+            
+        return ans
